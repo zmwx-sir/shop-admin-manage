fbbd9fd23446f8077921c034c8708a05b86a954b
“zouchen” <1434481913@qq.com>
2021-06-01 17:27:23 +0800

    第二天

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,107 @@
 <template>
-<div>
-    home组件
-</div>
+  <el-container class="home_container">
+    <el-header>
+      <div>
+        <img src="../../public/new_logo1_legal1.png" alt width="100" height="50" />
+        <span>货捕头电商管理系统</span>
+        
+      </div>
+      <el-button type="info" @click="login_out">退出</el-button>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu
+          class="el-menu-vertical-demo"
+          background-color="#313743"
+          text-color="#fff"
+          active-text-color="#409eff"
+        >
+          <el-submenu :index="item.id+''" v-for="item in MenuList" :key="item.id">
+            <!-- 一级菜单 的模板区域 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i :class="iconsObj[item.id]"></i>
+              <!-- 文本 -->
+              <span>{{item.authName}}</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item index="subItem.id+'" v-for="subItem in item.children" :key="subItem.id">
+              <template slot="title">
+                <!-- 图标 -->
+                <i class="el-icon-menu"></i>
+                <!-- 文本 -->
+                <span>{{subItem.authName}}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
-  name:'',
-  data(){
-   return {
-
-   }
+  name: "",
+  data() {
+    return {
+      MenuList: [],
+      iconsObj:{
+        "125":'iconfont icon-user',
+        "103":'iconfont icon-tijikongjian',
+        "101":'iconfont icon-shangpin',
+        "102":'iconfont icon-danju',
+        "145":'iconfont icon-baobiao',
+      }
+    };
+  },
+  created() {
+    this.getMenuList();
+  },
+  methods: {
+    login_out() {
+      window.sessionStorage.clear();
+      this.$router.push("/login");
+    },
+    //获取左侧菜单导航列表
+    async getMenuList() {
+      const { data: res } = await this.$http.get("menus");
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg);
+      this.MenuList = res.data;
+      console.log(res);
+    }
   }
-}
+};
 </script>
 
 <style scoped>
+.el-header {
+  background-color: #313743;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+}
 
+.el-header div{
+  display: flex;
+  align-items: center;
+}
+.el-header>div>span{
+  margin-left: 15px;
+}
+.el-aside {
+  background-color: #313743;
+}
+.el-main {
+  background-color: #eaedf1;
+}
+.home_container {
+  height: 100%;
+}
+.iconfont{
+  margin-right: 10px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -6,9 +6,16 @@ import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
 
 import axios from 'axios'
-Vue.prototype.$http=axios
-axios.defaults.baseURL='http://timemeetyou.com:8889/api/private/v1/'
-Vue.config.productionTip = false
+axios.defaults.baseURL='http://timemeetyou.com:8889/api/private/v1/';
+//axios请求拦截器
+axios.interceptors.request.use(
+  config=>{
+    config.headers.Authorization=window.sessionStorage.getItem('token');
+   return config;
+  }
+)
+Vue.prototype.$http=axios;
+Vue.config.productionTip = false;
 
 new Vue({
   router,
